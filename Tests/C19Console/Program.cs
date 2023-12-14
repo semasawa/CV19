845f486e8650c2fa847c68189674c9e075c59c7d
Semasawa <gusev_slava@list.ru>
2023-12-15 01:53:43 +0500

    Запрос к серверу для получения сырых данных

--- Tests/C19Console/Program.cs
+++ Tests/C19Console/Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using System.Net.Http;
 
 namespace C19Console
 {
     internal class Program
     {
+        private const string data_url = @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var client = new HttpClient();
+
+            var response = client.GetAsync(data_url).Result;
+            var csv_str = response.Content.ReadAsStringAsync().Result;
+
+            Console.ReadLine();
         }
     }
 }
