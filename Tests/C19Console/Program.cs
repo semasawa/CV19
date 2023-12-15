a388e313662188fd233d5ef4c8f1a36110f10bb0
Semasawa <gusev_slava@list.ru>
2023-12-15 23:47:22 +0500

    Тестирование бизнес-логики

--- Tests/C19Console/Program.cs
+++ Tests/C19Console/Program.cs
@@ -28,7 +28,8 @@ namespace C19Console
             {
                 var line = data_reader.ReadLine();
                 if (string.IsNullOrWhiteSpace(line)) continue;
-                yield return line;  
+
+                yield return line.Replace("Korea,", "Korea -");
             }
         }
 
@@ -38,6 +39,23 @@ namespace C19Console
             .Skip(4)
             .Select(s => DateTime.Parse(s, CultureInfo.InvariantCulture))
             .ToArray();
+
+        private static IEnumerable<(string Country, string Province, int[] Counts)> GetData()
+        {
+            var lines = GetDataLines()
+                .Skip(1)
+                .Select(line => line.Split(','));
+
+            foreach (var row in lines)
+            {
+                var province = row[0].Trim();
+                var country_name = row[1].Trim(' ', '"');
+                var counts = row.Skip(4).Select(int.Parse).ToArray();
+
+                yield return (country_name, province, counts);
+            }
+
+        }
         
         static void Main(string[] args)
         {
@@ -49,8 +67,13 @@ namespace C19Console
             //foreach (var data_line in GetDataLines())
             //    Console.WriteLine(data_line);
 
-            var dates = GetDates();
-            Console.WriteLine(string.Join("\r\n", dates));
+            //var dates = GetDates();
+            //Console.WriteLine(string.Join("\r\n", dates));
+             
+            var russia_data = GetData()
+                .First(v => v.Country.Equals("Russia", StringComparison.OrdinalIgnoreCase));
+
+            Console.WriteLine(string.Join("\r\n", GetDates().Zip(russia_data.Counts, (date, count) => $"{date:dd:MM} - {count}")));
 
             Console.ReadLine();
         }
